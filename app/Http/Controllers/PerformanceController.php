0bac467dcac7cbc844eb7b5a3c595492e5dc8341
rahat1795 <77228096+rahat1795@users.noreply.github.com>
2021-01-25 14:34:01 +0600

    Update PerformanceController.php

--- app/Http/Controllers/PerformanceController.php
+++ app/Http/Controllers/PerformanceController.php
@@ -17,70 +17,41 @@ class PerformanceController extends Controller
     {
         return view('performanceCreate');
     }
+    
+    public function bcreate()
+    {
+        return view('ploStore');
+    }
+    
     public function store( Request  $request)
     {
-        //return $request;
-//        $performance = new Performance([
-//            'user_id' => $request->get('student_id'),
-//            'course_id' => $request->get('course_id'),
-//            'section' => $request->get('section'),
-//            'semester' => $request->get('semester'),
-//            'm_co1_q1' => $request->get('m_co1_q1'),
-//            'm_co1_q2' => $request->get('m_co1_q2'),
-//            'm_co1_q3' => $request->get('m_co1_q3'),
-//            'm_co2_q4' => $request->get('m_co2_q4'),
-//            'm_co1_q5' => $request->get('m_co1_q5'),
-//            'm_co1_q6' => $request->get('m_co1_q6'),
-//            'semester_id' => $request->get('semester_id'),
-//            'session' => $request->get('session'),
-//        $name = $request->input('dept_code');
-//        $name2 = $request->input('dept_name');
-        $name3 = $request->input('plo_id');
-        $name4 = $request->input('student_id');
-        $name5 = $request->input('plo_number');
-        $name6 = $request->input('name');
-        $name7 = $request->input('question_id');
-        $name8 = $request->input('section_id');
-        $name9 = $request->input('co_id');
-        $name10 = $request->input('exam_type');
-        $name11 = $request->input('question_number');
-        $name12 = $request->input('marks');
-        $name13 = $request->input('answer_id');
-        $name14 = $request->input('marks_obtained');
-        $name15 = $request->input('isAchieved');
-//        $name16 = $request->input('course_code');
+        $name1 = $request->input('dept_code');
+        $name2 = $request->input('course_code');
+        $name3 = $request->input('co_id');
+        $name4 = $request->input('plo_id');
+        $name5= $request->input('plo_number');
 
 
-//        DB::table('department')->insert([
-//            'dept_code' => $name, 'dept_name' => $name2
-//        ]);
-        DB::table('plo')->insert([
-            'plo_id' => $name3, 'plo_number' => $name5,
-//            'dept_code' => $name,
-            'name' => $name6
-//            ->where('dept_code', $name)
+        DB::table('co')->insert([
+            'co_id' => $name3, 'course_code' => $name2, 'plo_id' => $name4
         ]);
-        DB::table('question_bank')->insert([
-            'question_id' => $name7,
-
-            'co_id' => $name9,
-//            'exam_type' => $name10,
-            'question_number' => $name11, 'marks' => $name12
-        ]);
-        DB::table('answer')->insert([
-            'answer_id' => $name13, 'marks_obtained' => $name14
-        ]);
-        DB::table('plo_track')->insert([
-            'student_id' => $name4,
-//            'course_code' => $name16,
-            'co_id' => $name9, 'plo_id' => $name3
-        ]);
-        DB::table('section')->insert([
-            'section_id' => $name8
-        ]);
-//        ]);
-//        $performance->save();
         return redirect()->back()->with('message', 'Data in submitted');
+    }
+    
+    public function ploStore( Request  $request)
+    {
+        $name1 = $request->input('dept_code');
+
+        $name2 = $request->input('name');
+        $name3 = $request->input('plo_id');
+        $name4 = $request->input('plo_number');
+
+
+        DB::table('plo')->insert([
+            'dept_code' => $name1, 'name' => $name2, 'plo_id' => $name3, 'plo_number' => $name4
+        ]);
+
+        return redirect()->back()->with('message', 'Data is submitted');
     }
 
     public function show( Request  $request)
